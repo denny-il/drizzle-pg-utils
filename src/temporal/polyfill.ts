b8bcb5687e37d1eeb1309cb3c6fd05e3a21074f9
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-06-19 16:28:19 +0700

    remove json access path alias

--- src/temporal/polyfill.ts
+++ src/temporal/polyfill.ts
@@ -7,31 +7,10 @@ import { createTimestamp } from './columns/timestamp.ts'
 import { createTimestampz } from './columns/timestampz.ts'
 import { createYearMonth } from './columns/year-month.ts'
 
-export const interval = createInterval(
-  // @ts-expect-error
-  Temporal,
-)
-export const monthDay = createMonthDay(
-  // @ts-expect-error
-  Temporal,
-)
-export const plainDate = createPlainDate(
-  // @ts-expect-error
-  Temporal,
-)
-export const time = createTime(
-  // @ts-expect-error
-  Temporal,
-)
-export const timestamp = createTimestamp(
-  // @ts-expect-error
-  Temporal,
-)
-export const timestampz = createTimestampz(
-  // @ts-expect-error
-  Temporal,
-)
-export const yearMonth = createYearMonth(
-  // @ts-expect-error
-  Temporal,
-)
+export const interval = createInterval(Temporal)
+export const monthDay = createMonthDay(Temporal)
+export const plainDate = createPlainDate(Temporal)
+export const time = createTime(Temporal)
+export const timestamp = createTimestamp(Temporal)
+export const timestampz = createTimestampz(Temporal)
+export const yearMonth = createYearMonth(Temporal)
